78dba0cd81974d16c682436a320666ad4cc272a9
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-20 13:58:07 -0300

    update types for testing

--- backend/src/types/auth-types.ts
+++ backend/src/types/auth-types.ts
@@ -1,10 +1,21 @@
-import type { AuthService } from "../services/auth-service.js";
+import { z } from 'zod';
 
+export interface IAuthService {
+  register: (data: RegisterPayload) => Promise<void>;
+  login: (...args: any) => Promise<any>;
+}
 export interface ControllerDeps {
-    authService: InstanceType<typeof AuthService>; 
+  authService: IAuthService;
 }
 
 export interface RegisterPayload {
-    email: string;
-    passwordHash: string;
+  email: string;
+  passwordHash: string;
 }
+
+export const registerSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
