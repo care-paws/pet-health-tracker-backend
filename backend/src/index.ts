4b571fa32a56deeef056945f526ffd9689e78571
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-24 12:31:45 -0300

    add cookie parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import apiRouter from './routes/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
 apiRouter(app);
 app.use(errorHandler);
