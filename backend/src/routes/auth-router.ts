5060930f47a20ac890d52b4896c0b6eb11277dc6
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-11-24 12:36:36 -0300

    update router

--- backend/src/routes/auth-router.ts
+++ backend/src/routes/auth-router.ts
@@ -7,5 +7,7 @@ const controller = authController({ authService });
 const router = express.Router();
 router.post('/register', controller.register);
 router.post('/login', controller.login);
+router.post('/logout', controller.logout);
+router.get('/currentUser', controller.getCurrentUser);
 
 export default router;
