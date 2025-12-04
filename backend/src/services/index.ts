78aaf5ea44f5cbac2c086e457a147f791b3e0584
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-12-04 16:45:32 -0300

    add reminders service

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -1,8 +1,10 @@
 import prisma from '../client.js';
 
 import { AuthService } from './auth-service.js';
+import { RemindersService } from './reminders-service.js';
 
 export const authService = new AuthService(prisma);
+export const remindersService = new RemindersService(prisma);
 
 import { PetService } from './pets-service.js';
 
@@ -11,4 +13,5 @@ export const petService = new PetService(prisma);
 import { EventService } from './event-services.js';
 
 export const eventService = new EventService(prisma);
+
 
