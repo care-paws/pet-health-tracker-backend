99ee902b3fdedf25b7d19765ff931927e1079ee3
kali <kali@kali.kali>
2025-12-05 10:58:37 -0500

    feat: implement CSRF protection middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import apiRouter from './routes/index.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import cookieParser from 'cookie-parser'
+import { csrfProtection } from './middlewares/csrf.js'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
+app.use(csrfProtection)
 apiRouter(app)
 app.use(errorHandler)
 
