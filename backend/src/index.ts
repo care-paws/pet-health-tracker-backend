579ac1b03e65123e826b8958dc724a75259dedee
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-12-12 10:46:48 -0300

    add cors configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,25 @@ import cors from 'cors'
 const PORT = process.env.PORT || 3000
 
 const app = express()
-app.use(cors())
+const allowedOrigins = [
+  'https://pet-health-tracker-frontend.vercel.app',
+  'http://localhost:5173', 
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if ( !origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token']
+  })
+);
 app.use(cookieParser())
 app.use(express.json())
 //app.use(csrfProtection)
